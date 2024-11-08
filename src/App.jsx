151802c13374dd1dc1f15617de46c3e767b00c71
raghav0807 <raghav.agiwal2002@gmail.com>
2024-11-08 06:13:37 +0530

    Completed Job-Portal Project

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import {
+  RouterProvider,
+  createBrowserRouter,
+  // ThemeProvider,
+} from "react-router-dom";
+
+import { ThemeProvider } from "./components/theme-provider";
+import "./App.css";
+// import { Button } from "./components/ui/button";
+import AppLayout from "./layouts/app-layout";
+import LandingPage from "./pages/landing";
+import Onboarding from "./pages/onboarding";
+import JobListing from "./pages/job-listing";
+import JobPage from "./pages/job";
+import PostJob from "./pages/post-job";
+import SavedJob from "./pages/saved-job";
+import MyJobs from "./pages/my-jobs";
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/onboarding",
+        element: <Onboarding />,
+      },
+      {
+        path: "/jobs",
+        element: <JobListing />,
+      },
+      {
+        path: "/job/:id",
+        element: <JobPage />,
+      },
+      {
+        path: "/post-job",
+        element: <PostJob />,
+      },
+      {
+        path: "/saved-jobs",
+        element: <SavedJob />,
+      },
+      {
+        path: "/my-jobs",
+        element: <MyJobs />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
+}
+
+export default App;
